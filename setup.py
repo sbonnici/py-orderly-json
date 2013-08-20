c4b14929094475d2d8e7a3b1c25ba65960833038
Stephen <stephenbonnici@gmail.com>
2013-08-20 09:08:05 +0200

    Created the setup file for the project and added a manifest template

---
+++ setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup
+
+setup(
+    name='orderlyjson',
+    version='0.1',
+    description='A python implementation of Orderly JSON '
+                'which includes a setup for easy installation',
+    url='http://github.com/sbonnici/py-orderly-json',
+    author='Stephen Bonnici',
+    author_email='stephen.bonnici@uniblue.com',
+    long_description=open('README.md').read(),
+    packages=[
+        'orderlyjson',
+        'orderlyjson.antlr3',
+        'orderlyjson.jsonschema',
+        'orderlyjson.jsonschema.tests'
+    ],
+    package_data={ 'orderlyjson': ['tools'] }
+)
